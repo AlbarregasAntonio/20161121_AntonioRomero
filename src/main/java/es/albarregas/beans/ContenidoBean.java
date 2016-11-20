3d303f6cd02a0574677c7aed7c9a9654e20a3da7
antonio-ACER <antonio.93.rs@gmail.com>
2016-11-21 00:19:34 +0100

    creada la aplicacion de la compañia de seguros

---
+++ src/main/java/es/albarregas/beans/ContenidoBean.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.beans;
+
+/**
+ *
+ * @author Antonio
+ */
+public class ContenidoBean {
+    private boolean daños;
+    private int valor;
+    private int franquicia;
+    private double prima;
+
+    public double getPrima() {
+        return prima;
+    }
+
+    public void setPrima(double prima) {
+        this.prima = prima;
+    }
+
+    public boolean isDaños() {
+        return daños;
+    }
+
+    public void setDaños(boolean daños) {
+        this.daños = daños;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getFranquicia() {
+        return franquicia;
+    }
+
+    public void setFranquicia(int franquicia) {
+        this.franquicia = franquicia;
+    }
+    
+}
---
+++ src/main/java/es/albarregas/beans/EdificioBean.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.beans;
+
+/**
+ *
+ * @author Antonio
+ */
+public class EdificioBean {
+    private String piso;
+    private int habitaciones;
+    private int fecha;
+    private String tipo;
+    private int valor;
+    private double prima;
+
+    public double getPrima() {
+        return prima;
+    }
+
+    public void setPrima(double prima) {
+        this.prima = prima;
+    }
+
+    public String getPiso() {
+        return piso;
+    }
+
+    public void setPiso(String piso) {
+        this.piso = piso;
+    }
+
+    public int getHabitaciones() {
+        return habitaciones;
+    }
+
+    public void setHabitaciones(int habitaciones) {
+        this.habitaciones = habitaciones;
+    }
+
+    public int getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(int fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    
+}
